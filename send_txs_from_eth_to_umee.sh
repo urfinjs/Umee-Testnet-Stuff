67d6242c50300f9e63db67868fb5ec080aa6baed
urfinjs <43937436+urfinjs@users.noreply.github.com>
2021-12-22 17:22:47 +0300

    Update send_txs_from_eth_to_umee.sh

--- send_txs_from_eth_to_umee.sh
+++ send_txs_from_eth_to_umee.sh
@@ -2,8 +2,8 @@
 source ~/.bash_profile
 
 amount_to_send="1"
-tx_to_send="50"
-delay_between_tx="15"
+txs_to_send="50"
+delay_between_txs="15"
 
 umee_erc20_contract="0xe54fbaecc50731afe54924c40dfd1274f718fe02"
 umee_wallet_addr=$UMEE_WALLET_ADDR_TASK
@@ -12,9 +12,9 @@ eth_pk=$ETH_PK_TASK
 
 log_file="${HOME}/$(date +"%y-%m-%d_%H-%M-%S")_eth_to_umee.txt"
 
-for (( i=1; i<=$tx_to_send; i++ )); do
+for (( i=1; i<=$txs_to_send; i++ )); do
   if [[ "$i" -ne "1" ]]; then
-      sleep $delay_between_tx
+      sleep $delay_between_txs
   fi
 
   # peggo bridge send-to-cosmos [token-address] [recipient] [amount] [flags]
@@ -24,7 +24,7 @@ for (( i=1; i<=$tx_to_send; i++ )); do
     # something went wrong
     echo "$result"
   else
-    echo "successful tx!" $(echo $result | grep -o 'Transaction: 0x.*' | awk '{printf $2}')
-    echo $(echo $result | grep -o 'Transaction: 0x.*' | awk '{printf $2}') >> $log_file
+    tx_hash=$(echo $result | grep -o 'Transaction: 0x.*' | awk '{printf $2}')
+    echo "successful tx! $tx_hash" && echo $tx_hash >> $log_file
   fi
 done
