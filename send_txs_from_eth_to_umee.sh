14f3b6c09fbadf537a321dd450c0652e7dfad4df
urfinjs <43937436+urfinjs@users.noreply.github.com>
2021-12-23 16:49:38 +0300

    Update send_txs_from_eth_to_umee.sh

--- send_txs_from_eth_to_umee.sh
+++ send_txs_from_eth_to_umee.sh
@@ -1,30 +1,56 @@
 #!/bin/bash
 source ~/.bash_profile
 
-amount_to_send="1"
-txs_to_send="50"
+amount_to_send="" # if string is empty then amount will be tx_number, eg. 53th tx will send 53uumee
+txs_to_send="200"
 delay_between_txs="15"
 
-umee_erc20_contract="0xe54fbaecc50731afe54924c40dfd1274f718fe02"
+eth_addr=$UMEE_WALLET_ETH_TASK
+umee_wallet_name=$UMEE_WALLET_TASK
 umee_wallet_addr=$UMEE_WALLET_ADDR_TASK
-eth_rpc=$ETH_RPC
-eth_pk=$ETH_PK_TASK
+umee_chain=$UMEE_CHAIN
+umee_pass=$UMEE_PASS
+gas_flags="--gas=auto --gas-adjustment=1.4"
+extra_flags=""  # --node, --home etc
 
-log_file="${HOME}/$(date +"%y-%m-%d_%H-%M-%S")_eth_to_umee.txt"
+log_file="${HOME}/$(date +"%y-%m-%d_%H-%M-%S")_umee_to_eth.txt"
+
+
+send_in_increment=0
+if [ -z "$amount_to_send" ]; then
+  send_in_increment=1
+fi
+
 
 for (( i=1; i<=$txs_to_send; i++ )); do
   if [[ "$i" -ne "1" ]]; then
       sleep $delay_between_txs
   fi
 
-  # peggo bridge send-to-cosmos [token-address] [recipient] [amount] [flags]
-  result=$(peggo bridge send-to-cosmos $umee_erc20_contract $umee_wallet_addr $amount_to_send --eth-pk=$eth_pk --eth-rpc=$eth_rpc --cosmos-chain-id=$umee_chain 2>&1)
+  # path to seq differ for each project; examples: `.base_account.sequence`, `.sequence`
+  seq=$(umeed q account $umee_wallet_addr $extra_flags -o=json | jq -r .sequence)
+  error_code="32"
 
-  if [ -z "$(echo $result | grep -o successfully)" ]; then
-    # something went wrong
-    echo "$result"
-  else
-    tx_hash=$(echo $result | grep -o 'Transaction: 0x.*' | awk '{printf $2}')
-    echo "successful tx! $tx_hash" && echo $tx_hash >> $log_file
+  if [[ "$send_in_increment" -eq "1" ]]; then
+    amount_to_send=$i
   fi
+
+  while [[ "$error_code" != "0" ]]; do
+    # umeed tx peggy send-to-eth [eth-dest-addr] [amount] [bridge-fee] [flags]
+    result=$(echo $umee_pass | umeed tx peggy send-to-eth $eth_addr ${amount_to_send}uumee 1uumee --from=$umee_wallet_name --chain-id=$umee_chain $gas_flags $extra_flags -y -o=json)
+    error_code=$(echo $result | jq .code)
+
+    if [[ "$error_code" == "32" ]]; then
+      # get account sequence from the failed tx raw_log
+      seq=$(echo ${result} | jq .raw_log | awk '{print substr($5, 1, length($5)-1)}')
+      echo "wrong sequence; right sequence is:" $seq & sleep 1
+    elif [[ "$error_code" != "0" ]]; then
+      # path to seq differ for each project; examples: `.base_account.sequence`, `.sequence`
+      seq=$(umeed q account $umee_wallet_addr $extra_flags -o=json | jq -r .sequence)
+      echo "error code:" $error_code & sleep 1
+    else
+      tx_hash=$(echo $result | jq -r .txhash)
+      echo "successful tx! $tx_hash" && echo $tx_hash >> $log_file
+    fi
+  done
 done
